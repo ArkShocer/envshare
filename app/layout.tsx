4ecb45358c908078ef01177ab4a8f2b8c5457c86
Daniel <daniel.sulzer00@gmx.at>
2023-03-24 15:07:53 +0100

    minor change

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={inter.variable}>
       <head />
-      <body className="relative min-h-80vh bg-black bg-gradient-to-tr from-zinc-900/50 to-zinc-700/30">
+      <body className="relative min-h-screen bg-black bg-gradient-to-tr from-zinc-900/50 to-zinc-700/30">
         <Header />
 
         <main className=" min-h-[80vh] ">{children}</main>
